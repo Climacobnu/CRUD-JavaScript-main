4ebf50ef77ba16eded6b7bb05bf33dd6dbb2c78f
Leandro Climaco <climacobnu@gmail.com>
2022-07-27 17:29:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -96,6 +96,30 @@ const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
 loadItens()
 
 
+// function myFunction() {
+//   // Declare variables
+//   var input, filter, table, tr, td, i, txtValue;
+//   input = document.getElementById("myInput");
+//   filter = input.value.toUpperCase();
+//   table = document.getElementById("myTable");
+//   tr = table.getElementsByTagName("tr");
+
+//   // Loop through all table rows, and hide those who don't match the search query
+//   for (i = 0; i < tr.length; i++) {
+//     td = tr[i].getElementsByTagName("td")[0];
+//      if (td) {
+//       txtValue = td.textContent || td.innerText;
+//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//         tr[i].style.display = "";
+//       } else {
+//         tr[i].style.display = "none";
+//       }
+//     }
+//   }
+// }
+
+
+
 function myFunction() {
   // Declare variables
   var input, filter, table, tr, td, i, txtValue;
@@ -106,13 +130,19 @@ function myFunction() {
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-     if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
+    if (!tr[i].classList.contains('header')) {
+      td = tr[i].getElementsByTagName("td"),
+      match = false;
+      for (j = 0; j < td.length; j++) {
+        if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+          match = true;
+          break;
+        }
+      }
+      if (!match) {
         tr[i].style.display = "none";
+      } else {
+        tr[i].style.display = "";
       }
     }
   }
