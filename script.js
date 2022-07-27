0d6164755eedfecdf37f1637a80e021d4538b8bf
Leandro Climaco <climacobnu@gmail.com>
2022-07-27 18:27:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -152,3 +152,10 @@ function myFunction() {
 
 
 
+async function saveFile() {
+  let formData = new FormData();
+  formData.append("file", fileupload.files[0]);
+  await fetch('/upload.php' , {method: "POST", body: formData});
+  alert('The file has been uploaded sucessfuly');
+
+}
